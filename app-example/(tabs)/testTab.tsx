97e57faff3b9244884147004c5b2f18820ebd1f8
devkiseok <dev.leekiseok@gmail.com>
2024-09-11 17:46:22 +0900

    feat : 라우팅 그룹 파일 구조 적용

---
+++ app-example/(tabs)/testTab.tsx
@@ -0,0 +1,31 @@
+import { HelloWave } from "@/components/HelloWave";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet } from "react-native";
+
+export default function TestTab() {
+  return (
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">test Tab</ThemedText>
+      <HelloWave />
+    </ThemedView>
+  );
+}
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
